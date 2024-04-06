3b56d7ee45655871ad1c241095c82ab009c0e226
Cruelness55 <molnarmate504@gmail.com>
2024-04-06 19:15:12 +0200

    z

--- assets/js/bounce.js
+++ assets/js/bounce.js
@@ -1,26 +1,8 @@
-var 
-    myText = document.getElementById("bounceTxt").innerHTML,
-    wrapText = "";
+var myText = document.getElementById("bounceTxt").innerHTML;
+var wrapText = "";
 
-for (var i=0; i<myText.length; i++) {
-     wrapText += "<em>" + myText.charAt(i) + "</em>";
+for (var i = 0; i < myText.length; i++) {
+    wrapText += "<span class='bounce-me'>" + myText.charAt(i) + "</span>";
 }
 
 document.getElementById("bounceTxt").innerHTML = wrapText;
-
-var 
-    myLetters = document.getElementsByTagName("em"),
-    j = 0;
-
-function applyBounce() {
-     setTimeout(function() {
-          myLetters[j].className = "bounce-me";
-          j++;
-          
-          if (j < myLetters.length) {
-               applyBounce();
-          }
-     }, 250);
-}
-
-applyBounce();
